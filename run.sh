572f59bd4500d5e84c1c944c56ad7d1b93558570
ryhkml <reyhank95@hotmail.com>
2022-12-29 16:44:35 +0700

    build: cleanup script

--- run.sh
+++ run.sh
@@ -13,53 +13,68 @@ DOMAIN=$1
 DURATION=$2
 
 if [[ -z $DOMAIN || -z $DURATION ]]; then
-  echo "${ERROR}SEMUA ARGUMEN HARUS DIISI"
+  error "SEMUA ARGUMEN HARUS DIISI"
   exit 1
 fi
 
-function throw_err() {
-  echo "${ERROR}TERJADI KESALAHAN"
+info() {
+  local TEXT="${INFO}$1${NORMAL}"
+  echo $TEXT
+}
+
+link() {
+  local TEXT="${LINK}$1${NORMAL}"
+  echo $TEXT
+}
+
+error() {
+  local TEXT="${ERROR}$1${NORMAL}"
+  echo $TEXT
+}
+
+throw_error() {
+  error "TERJADI KESALAHAN"
   rm -rf ./$DOMAIN
   exit 1
 }
 
 echo
-echo "${INFO}OK${NORMAL}"
+info "MULAI"
 
 openssl version
 
 mkdir $DOMAIN
 
 echo
-echo "${INFO}BUAT PASSWORD PEM PHRASE, TUNGGU SEBENTAR...${NORMAL}"
+info "BUAT PASSWORD PEM PHRASE, TUNGGU SEBENTAR..."
 
 sleep 1s
 
 openssl genrsa -aes256 \
   -out ./$DOMAIN/ca-key.pem \
-  4096 || throw_err
+  4096 || throw_error
 
 echo
-echo "${INFO}VERIFIKASI KEMBALI PASSWORD PEM PHRASE${NORMAL}"
+info "VERIFIKASI KEMBALI PASSWORD PEM PHRASE"
 
 openssl req -new -x509 -sha256 \
   -days $DURATION \
   -key ./$DOMAIN/ca-key.pem \
-  -out ./$DOMAIN/ca.pem || throw_err
+  -out ./$DOMAIN/ca.pem || throw_error
 
 openssl genrsa \
   -out ./$DOMAIN/cert-key.pem \
-  4096 || throw_err
+  4096 || throw_error
 
 openssl req -new -sha256 \
   -subj "/CN=$DOMAIN" \
   -key ./$DOMAIN/cert-key.pem \
-  -out ./$DOMAIN/cert.csr || throw_err
+  -out ./$DOMAIN/cert.csr || throw_error
 
 echo "subjectAltName=DNS:$DOMAIN" > ./$DOMAIN/extfile.cnf
 
 echo
-echo "${INFO}OK, VERIFIKASI KEMBALI PASSWORD PEM PHRASE${NORMAL}"
+info "OK, VERIFIKASI KEMBALI PASSWORD PEM PHRASE"
 
 openssl x509 -req -sha256 \
   -days $DURATION \
@@ -68,13 +83,21 @@ openssl x509 -req -sha256 \
   -CAkey ./$DOMAIN/ca-key.pem \
   -out ./$DOMAIN/cert.pem \
   -extfile ./$DOMAIN/extfile.cnf \
-  -CAcreateserial || throw_err
+  -CAcreateserial || throw_error
 
 cat ./$DOMAIN/cert.pem > ./$DOMAIN/fullchain.pem
 cat ./$DOMAIN/ca.pem >> ./$DOMAIN/fullchain.pem
 
+# Optional step
 echo
-echo "${INFO}SELESAI${NORMAL}"
+info "VERIFIKASI CA FILE"
+
+openssl verify \
+  -CAfile ./$DOMAIN/ca.pem ./$DOMAIN/cert.pem || throw_error
+
+# End
+echo
+info "SELESAI"
 echo "key=$DOMAIN/cert-key.pem"
 echo "csr/cert=$DOMAIN/fullchain.pem"
 echo
@@ -82,7 +105,7 @@ echo "AGAR PROTOKOL HTTPS TIDAK DICORET OLEH BROWSER"
 echo "LANGKAH TERAKHIR, MASUKKAN SERTIFIKAT KE DALAM BROWSER"
 echo
 echo "Google Chrome"
-echo "${LINK}https://support.google.com/chrome/a/answer/3505249?hl=en${NORMAL}"
+link "https://support.google.com/chrome/a/answer/3505249?hl=en"
 echo
 echo "Mozilla Firefox"
-echo "${LINK}https://docs.vmware.com/en/VMware-Adapter-for-SAP-Landscape-Management/2.1.0/Installation-and-Administration-Guide-for-VLA-Administrators/GUID-0CED691F-79D3-43A4-B90D-CD97650C13A0.html${NORMAL}"
+link "https://docs.vmware.com/en/VMware-Adapter-for-SAP-Landscape-Management/2.1.0/Installation-and-Administration-Guide-for-VLA-Administrators/GUID-0CED691F-79D3-43A4-B90D-CD97650C13A0.html"
